cdf2fdfd567e0e0181c6779b3f28c6534d30fe29
younusraza909 <younusraza909@gmail.com>
2025-01-28 07:49:06 +0500

    Root layout setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/assets/styles/global.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Prostore",
-  description: "A modern e-commerce platform built with Next.js",
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: `${APP_NAME}`,
+  },
+  description: `${APP_DESCRIPTION}`,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
