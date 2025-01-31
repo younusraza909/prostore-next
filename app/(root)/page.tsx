f33548763364549e424209cc617e1cc2509a80be
younusraza909 <younusraza909@gmail.com>
2025-01-31 08:30:56 +0500

    Add Product related component basic from Shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Home = () => {
-  return <>Prostore</>;
+  return (
+    <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+  );
 };
 
 export default Home;
