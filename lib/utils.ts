cdf509919bacfd7f9af424a7fc4fe3f3686d5751
younusraza909 <younusraza909@gmail.com>
2025-02-03 08:12:48 +0500

    Added type safety Product

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// Format Number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+}
+
 // Convert prisma object into a regular JS object
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
